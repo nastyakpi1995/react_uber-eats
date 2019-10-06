be5be7425525594074c38d71212d6422e850b75a
Nastia Krat <nastyakpi1995@gmail.com>
2019-10-06 17:49:13 +0300

    Updates

---
+++ precache-manifest.93a3515336daacd563875fcf43033f87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1654896452bf4bcd3c8975f09a4dd096",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "13f839e5e274806fbfd4",
+    "url": "/react_uber-eats/static/css/main.57c39f9d.chunk.css"
+  },
+  {
+    "revision": "8fa2e2175575b798d51a",
+    "url": "/react_uber-eats/static/js/2.148abb38.chunk.js"
+  },
+  {
+    "revision": "13f839e5e274806fbfd4",
+    "url": "/react_uber-eats/static/js/main.7884bc29.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  },
+  {
+    "revision": "76271051cc6514b1f069254911cda60a",
+    "url": "/react_uber-eats/static/media/planet.76271051.svg"
+  },
+  {
+    "revision": "d4cd7e36f140e2dc4405377b4b486a0e",
+    "url": "/react_uber-eats/static/media/search.d4cd7e36.svg"
+  },
+  {
+    "revision": "6c198fbe2c21523303bdd8b8113691cc",
+    "url": "/react_uber-eats/static/media/time.6c198fbe.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.bf7d65e78b2cca39084017d8fc18de03.js"
+  "/react_uber-eats/precache-manifest.93a3515336daacd563875fcf43033f87.js"
 );
 
 self.addEventListener('message', (event) => {
